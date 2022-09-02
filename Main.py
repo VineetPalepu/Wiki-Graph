7b9ecc34531ce5c3e1edba13b39ea02d5c8a8d0c
VineetPalepu <vineetmpalepu@gmail.com>
2022-09-02 00:31:52 -0700

    added page retrieval by article name

--- Main.py
+++ Main.py
@@ -1,4 +1,10 @@
+import bz2
+from distutils.command.config import dump_file
+from pkgutil import get_data
+
 index_file_name = "enwiki-20211020-pages-articles-multistream-index.txt"
+dump_file_name = "enwiki-20211020-pages-articles-multistream.xml.bz2"
+block_size = 262144
 
 def get_article_offset_id(article_title):
     with open(r"E:\Data" + "\\" + index_file_name, encoding="utf-8") as f:
@@ -8,12 +14,67 @@ def get_article_offset_id(article_title):
             title = data[2].strip()
             offset = int(data[0])
             id = int(data[1])
-
-            print(article_title, title)
             if article_title == title:
-                print("Found Article")
+                print("Found Article:", offset, id)
                 return (offset, id)
-            input()
         print("Invalid Article")
+        raise
 
-print(get_article_offset_id("AccessibleComputing"))
+def get_article(article_title):
+    offset, id = get_article_offset_id(article_title)
+    with open(r"E:\Data" + "\\" + dump_file_name, mode="rb") as f:
+        f.seek(offset)
+        d = bz2.BZ2Decompressor()
+        # reads the bytes of the entire stream (100 articles) and then decompresses them
+        block = f.read(block_size)
+        data = d.decompress(block)
+        prev_data = data # maybe use for the off chance that the header of the xml gets split in two blocks
+        while data.find("<title>{}</title>".format(article_title).encode()) == -1:
+            block = f.read(block_size)
+            data = d.decompress(block)
+
+        article_title_index = data.find("<title>{}</title>".format(article_title).encode())
+
+        start = article_title_index
+        while data.find("</page>".encode(), start) == -1:
+            block = f.read(block_size)
+            data.append(d.decompress(block))
+            start = 0
+
+        article_end_index = data.find("</page>".encode(), article_title_index) + len("</page>".encode())
+
+        article_start_index = data.rfind("<page>".encode(), 0, article_title_index)
+        if article_start_index == -1:
+            print("was really hoping to not see this message cause the fix is annoying and i'm lazy")
+            raise
+
+        article_data = data[article_start_index:article_end_index]
+
+        wtf("out.xml", article_data)
+
+        return article_data
+        
+
+# write to file
+def wtf(filename, data):
+    with open(filename, mode="wb") as f:
+        f.write(data)
+
+xml = get_article("Polar coordinate system")
+
+
+
+"""
+with open(r"E:\Data" + "\\" + dump_file_name, mode="rb") as f:
+f.seek(163132176, 0)
+d = bz2.BZ2Decompressor()
+
+block = f.read(block_size)
+data = d.decompress(block)
+
+block2 = f.read(block_size)
+data2 = d.decompress(block2)
+
+wtf("out1.xml", data)
+wtf("out2.xml", data2)
+"""
