8f02890854e32be1c2fcdaff180f45afee97b258
VineetPalepu <vineetmpalepu@gmail.com>
2022-09-01 21:10:54 -0700

    added index searching for article names

--- Main.py
+++ Main.py
@@ -1,11 +1,19 @@
 index_file_name = "enwiki-20211020-pages-articles-multistream-index.txt"
 
+def get_article_offset_id(article_title):
+    with open(r"E:\Data" + "\\" + index_file_name, encoding="utf-8") as f:
+        lines = 0
+        for line in f:
+            data = line.split(":")
+            title = data[2].strip()
+            offset = int(data[0])
+            id = int(data[1])
 
-wiki_index = open(r"E:\Data" + "\\" + index_file_name)
+            print(article_title, title)
+            if article_title == title:
+                print("Found Article")
+                return (offset, id)
+            input()
+        print("Invalid Article")
 
-
-print(wiki_index.readline())
-print(wiki_index.readline())
-print(wiki_index.readline())
-print(wiki_index.readline())
-print(wiki_index.readline())
+print(get_article_offset_id("AccessibleComputing"))
