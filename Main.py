e7de88adc900c7441aa6ebbc14daa19d6e702dc7
VineetPalepu <vineetmpalepu@gmail.com>
2022-09-01 19:56:47 -0700

    Read first lines of index

---
+++ Main.py
@@ -0,0 +1,11 @@
+index_file_name = "enwiki-20211020-pages-articles-multistream-index.txt"
+
+
+wiki_index = open(r"E:\Data" + "\\" + index_file_name)
+
+
+print(wiki_index.readline())
+print(wiki_index.readline())
+print(wiki_index.readline())
+print(wiki_index.readline())
+print(wiki_index.readline())
