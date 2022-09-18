8af2f5f9f90eca0e03789309a59031bd43960a41
Vineet Palepu <vineetmpalepu@gmail.com>
2022-09-17 19:13:47 -0700

    Use string search to find article title for better performance

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,7 +1,9 @@
-use std::{path::Path, fs::File, io::{Read, BufReader, BufRead}, time::Instant};
+use std::{path::Path, fs::{File, self, read_dir}, io::{Read, BufReader, BufRead}, time::Instant};
 
-fn main() {
-    let article = "CrCs2O4";
+// TODO: Make a hierarchical wiki browser, i.e. when you click a link it keeps track of which article you came from and when you finish an article it goes back to the parent article
+
+fn main() {    
+    let article = "Category:Japan rail succession modules";
     let t1 = Instant::now();
     let result = get_article_offset_id(article);
     println!("{:?} seconds to search index", t1.elapsed());
@@ -24,8 +26,11 @@ struct IndexData
 
 fn get_article_offset_id(article_title: &str) -> Option<IndexData>
 {
-    let path = Path::new("E:\\Data\\enwiki-20211020-pages-articles-multistream-index.txt");
-    let mut index_file = match File::open(path)
+    let path = Path::new(r"C:\Users\Vineet Palepu\Downloads\enwiki-20220101-pages-articles-multistream\enwiki-20220101-pages-articles-multistream-index.txt");
+
+    println!("Opening index located at {}", path.display());
+
+    let index_file = match File::open(path)
     {
         Ok(file) => file,
         Err(e) => panic!("couldn't open {}: {}", path.display(), e),
@@ -34,16 +39,18 @@ fn get_article_offset_id(article_title: &str) -> Option<IndexData>
 
     for article_data in index_file.lines()
     {
+        
         let article_data = article_data.unwrap();
-        let data: Vec<&str> = article_data.split(":").collect();
-
-        let offset: usize = data[0].parse().expect("couldn't parse article byte offset");
-        let id: usize = data[1].parse().expect("couldn't parse article ID");
-        let title = data[2].trim();
-
-        if title == article_title
+        if article_data.contains(article_title)
         {
+            let data: Vec<&str> = article_data.split(":").collect();
+
+            let offset: usize = data[0].parse().expect("couldn't parse article byte offset");
+            let id: usize = data[1].parse().expect("couldn't parse article ID");
+            let title = data[2].trim();
+
             return Some(IndexData{offset, id});
+
         }
     }
 
