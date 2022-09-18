44b0c5049c15795c32f4678781376ea0548f1cc7
Vineet Palepu <vineetmpalepu@gmail.com>
2022-09-17 23:33:10 -0700

    remove unneeded imports

--- wiki_graph/src/main.rs
+++ wiki_graph/src/main.rs
@@ -1,13 +1,8 @@
 // TODO: Make a hierarchical wiki browser, i.e. when you click a link it keeps track of which article you came from and when you finish an article it goes back to the parent article
 
-use std::time::Instant;
-
 fn main() {
     let article = "OpenHistoricalMap";
-    let t1 = Instant::now();
     let result = wiki_graph::get_article_offset_id(article);
-    println!("{:?} seconds to search index", t1.elapsed());
-
     match result {
         Some(data) => {
             println!(
