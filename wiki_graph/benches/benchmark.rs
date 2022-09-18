b373bb213d3cf75920017ae3e133a5545064c76a
Vineet Palepu <vineetmpalepu@gmail.com>
2022-09-18 14:58:05 -0700

    added benchmark for geting article data from index

--- wiki_graph/benches/benchmark.rs
+++ wiki_graph/benches/benchmark.rs
@@ -10,8 +10,9 @@ macro_rules! benchmark {
 }
 
 fn main() {
-    bench_build_index();
     bench_count_lines();
+    bench_build_index();
+    bench_get_article_offset_id_from_index();
     bench_get_article_offset_id();
 }
 
@@ -54,4 +55,30 @@ fn bench_build_index()
     let index_file = format!("{}{}", dir, index_file);
 
     benchmark!(build_index(&index_file));
+}
+
+fn bench_get_article_offset_id_from_index()
+{
+    let dir = r"C:\Users\Vineet Palepu\Downloads\enwiki-20220101-pages-articles-multistream\";
+    let index_file = "enwiki-20220101-pages-articles-multistream-index.txt";
+    let index_file = format!("{}{}", dir, index_file);
+
+    let articles = [
+        "ArtificalLanguages",
+        "Wireless application service provider",
+        "TheFilmSchool",
+        "Vahid Mirzadeh",
+        "William Loren Batt",
+        "Buddhist Tripitaka",
+        "Blight remediation",
+        "Town of Brookhaven",
+        "Floppy disk interface",
+        "OpenHistoricalMap",
+    ];
+
+    let index = build_index(&index_file);
+
+    for article in articles {
+        benchmark!(get_article_offset_id_from_index(&index, article));
+    }
 }
