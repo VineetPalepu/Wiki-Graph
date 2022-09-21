e8e0ef3250982d39bc0a9c95a8f528a61030f9aa
Vineet Palepu <vineetmpalepu@gmail.com>
2022-09-20 23:09:46 -0700

    remove old benchmark code

--- wiki_graph/benches/benchmark.rs
+++ wiki_graph/benches/benchmark.rs
@@ -15,52 +15,5 @@ macro_rules! benchmark {
 
 fn main()
 {
-    bench_count_lines();
-    bench_build_index();
-    bench_get_article_offset_id_from_index();
-}
-
-fn bench_count_lines()
-{
-    let dir = r"data";
-    let index_file = "enwiki-20220101-pages-articles-multistream-index.txt";
-    let index_file = format!("{dir}\\{index_file}");
-
-    benchmark!(count_lines(&Path::new(&index_file)));
-}
-
-fn bench_build_index()
-{
-    let dir = r"data";
-    let index_file = "enwiki-20220101-pages-articles-multistream-index.txt";
-    let index_file = format!("{dir}\\{index_file}");
-
-    benchmark!(build_index(&Path::new(&index_file)));
-}
-
-fn bench_get_article_offset_id_from_index()
-{
-    let dir = r"data";
-    let index_file = "enwiki-20220101-pages-articles-multistream-index.txt";
-    let index_file = format!("{dir}\\{index_file}");
-
-    let articles = [
-        "ArtificalLanguages",
-        "Wireless application service provider",
-        "TheFilmSchool",
-        "Vahid Mirzadeh",
-        "William Loren Batt",
-        "Buddhist Tripitaka",
-        "Blight remediation",
-        "Town of Brookhaven",
-        "Floppy disk interface",
-        "OpenHistoricalMap",
-    ];
-
-    let index = build_index(&Path::new(&index_file));
-
-    for article in articles
-    {
-        benchmark!(get_article_offset_id(&index, article));
-    }
+    //TODO: add benchmarks
 }
